1d2bc845d7b1f36b6c35b6e39feaced390dfb627
Sam Phung <phungnguyenphuong01@gmail.com>
2026-07-07 20:28:37 +1000

    Finish prelim bar graphs for gender gaps. Overall table remaining

--- Rscripts/data_viz.R
+++ Rscripts/data_viz.R
@@ -9,10 +9,10 @@ pacman::p_load(tidyverse, # for data wrangling + visualisation
 # Import cleaned data set ----
 load(here::here("data", "pay_clean.RData"))
 
-# Create new variable for TotalPay = BasePay + Bonus ----
-pay_clean <- pay_clean |>
-  mutate(TotalPay = BasePay + Bonus)
-attr(pay_clean$TotalPay, "label") <- "Tổng lương (USD)"
+# Create new variable for TotalPay = BasePay + Bonus 
+# pay_clean <- pay_clean |>
+#   mutate(TotalPay = BasePay + Bonus)
+# attr(pay_clean$TotalPay, "label") <- "Tổng lương (USD)"
 
 # Descriptive stats
 psych::describe(pay_clean)
@@ -43,7 +43,7 @@ patchwork1 + plot_annotation(title = "Phân bố Lương cơ bản và Lương t
 
 ## Box plots ----
 p3 <- ggplot(pay_clean, aes(BasePay, Gender)) +
-  stat_boxplot(geom = "errorbar", width = 0.2) +
+  stat_boxplot(geom = "errorbar", width = 0.1) +
   geom_boxplot(fill = "lightgrey") +
   theme(
     panel.background = element_blank(),
@@ -51,7 +51,7 @@ p3 <- ggplot(pay_clean, aes(BasePay, Gender)) +
   ) 
 
 p4 <- ggplot(pay_clean, aes(Bonus, Gender)) +
-  stat_boxplot(geom = "errorbar", width = 0.2) +
+  stat_boxplot(geom = "errorbar", width = 0.1) +
   geom_boxplot(fill = "lightgrey") +
   theme(
     panel.background = element_blank(),
@@ -62,30 +62,30 @@ patchwork2 <- (p3 / p4) + plot_layout(axis_titles = "collect") | plot_spacer()
 patchwork2 + plot_annotation(title = "Phân bố Lương cơ bản và Lương thưởng theo Giới tính")
 
 ## Table of of overall mean pay and pay gap (UNFINISHED) ----
-pay_clean |>
-  summarise(meanBase = mean(BasePay),
-            meanBonus = mean(Bonus),
-            .by = Gender) |>
-  pivot_wider(names_from = Gender,
-              values_from = c(meanBase, meanBonus)) |>
-  mutate(base_gap = round(((meanBase_Male - meanBase_Female) / meanBase_Male * 100), 2),
-         bonus_gap = round(((meanBonus_Male - meanBonus_Female) / meanBonus_Male * 100), 2)) |>
-  pivot_longer(cols = c("meanBase_Male", "meanBonus_Male"),
-               names_to = "Type",
-               values_to = "Male",
-               values_drop_na = T) |>
-  pivot_longer(cols = c("meanBase_Female", "meanBonus_Female"),
-               names_to = "Type1",
-               values_to = "Female") 
-
-overall_gap
-
-overall_gap |>
-  pivot_longer(cols = c("base_gap", "bonus_gap"),
-               names_to = "Gap",
-               values_to = "Type2")
-
-  gt() 
+# pay_clean |>
+#   summarise(meanBase = mean(BasePay),
+#             meanBonus = mean(Bonus),
+#             .by = Gender) |>
+#   pivot_wider(names_from = Gender,
+#               values_from = c(meanBase, meanBonus)) |>
+#   mutate(base_gap = round(((meanBase_Male - meanBase_Female) / meanBase_Male * 100), 2),
+#          bonus_gap = round(((meanBonus_Male - meanBonus_Female) / meanBonus_Male * 100), 2)) |>
+#   pivot_longer(cols = c("meanBase_Male", "meanBonus_Male"),
+#                names_to = "Type",
+#                values_to = "Male",
+#                values_drop_na = T) |>
+#   pivot_longer(cols = c("meanBase_Female", "meanBonus_Female"),
+#                names_to = "Type1",
+#                values_to = "Female") 
+# 
+# overall_gap
+# 
+# overall_gap |>
+#   pivot_longer(cols = c("base_gap", "bonus_gap"),
+#                names_to = "Gap",
+#                values_to = "Type2")
+# 
+#   gt() 
   # tab_header(
   #   title = "Trung bình Lương cơ bản theo Giới tính và Chênh lệch (%)") |>
   # tab_footnote(
@@ -122,19 +122,19 @@ pay_clean$AgeGroup = cut(pay_clean$Age, breaks = c(18, 25, 35, 45, 55, 65, Inf),
 attr(pay_clean$AgeGroup, "label") <- "Nhóm tuổi"
 
 # Compute pay gap 
-meanbase_age <- pay_clean |>
+gendergap_age <- pay_clean |>
   summarise(mean = mean(BasePay),
             .by = c(AgeGroup, Gender)) |>
   pivot_wider(names_from = Gender,
               values_from = mean) |>
   mutate(base_gap = (Male - Female) / Male)
 
-meanbonus_age <- pay_clean |>
-  summarise(mean = mean(Bonus),
-            .by = c(AgeGroup, Gender)) |>
-  pivot_wider(names_from = Gender,
-              values_from = mean) |>
-  mutate(bonus_gap = (Male - Female) / Male)
+# meanbonus_age <- pay_clean |>
+#   summarise(mean = mean(Bonus),
+#             .by = c(AgeGroup, Gender)) |>
+#   pivot_wider(names_from = Gender,
+#               values_from = mean) |>
+#   mutate(bonus_gap = (Male - Female) / Male)
 
 # meantotal_age <- pay_clean |>
 #   summarise(mean = mean(TotalPay),
@@ -144,57 +144,129 @@ meanbonus_age <- pay_clean |>
 #   mutate(total_gap = (Male - Female) / Male)
 
 # Create bar plots
-p5 <- ggplot(meanbase_age, aes(x = AgeGroup, y = base_gap, label = percent(base_gap))) +
+p5 <- ggplot(gendergap_age, aes(x = AgeGroup, y = base_gap, label = percent(base_gap))) +
   geom_col() +
-  geom_text(nudge_y = 0.01) +
+  geom_text(nudge_y = 0.005) +
   scale_y_continuous(breaks = seq(0, 1, by = 0.05), 
                      limits = c(0, 0.2),
                      expand = c(0, 0),
                      labels = label_percent(),
                      name = "Chênh lệch (%)") +
-  labs(title = "Chênh lệch về Lương cơ bản") +
+  labs(title = "Chênh lệch về Lương cơ bản giữa Nam và Nữ theo Nhóm tuổi",
+       subtitle = "Chênh lệch = (Lương TB của Nam - Lương TB của Nữ) / Lương TB của Nam * 100%") +
   theme(
     panel.border = element_rect(colour = "black"),
     panel.background = element_blank()
   )
 
-p6 <- ggplot(meanbonus_age, aes(x = AgeGroup, y = bonus_gap)) +
-  geom_col() +
-  scale_y_continuous(breaks = seq(-0.2, 0.2, by = 0.05), 
-                     limits = c(-0.2, 0.2),
-                     expand = c(0, 0),
-                     labels = label_percent(),
-                     name = "Chênh lệch (%)") +
-  geom_text(aes(y = bonus_gap + 0.02*sign(bonus_gap) ,
-                label = percent(bonus_gap))) +
-  labs(title = "Chênh lệch về Lương thưởng") +
-  theme(
-    panel.border = element_rect(colour = "black"),
-    panel.background = element_blank()
-  )
+p5
 
-patchwork3 <- (p5 / p6) + plot_layout(axis_titles = "collect") | plot_spacer()
+# p6 <- ggplot(gendergap_age, aes(x = AgeGroup, y = bonus_gap)) +
+#   geom_col() +
+#   scale_y_continuous(breaks = seq(-0.2, 0.2, by = 0.05), 
+#                      limits = c(-0.2, 0.2),
+#                      expand = c(0, 0),
+#                      labels = label_percent(),
+#                      name = "Chênh lệch (%)") +
+#   geom_text(aes(y = bonus_gap + 0.02*sign(bonus_gap) ,
+#                 label = percent(bonus_gap))) +
+#   labs(title = "Chênh lệch về Lương thưởng") +
+#   theme(
+#     panel.border = element_rect(colour = "black"),
+#     panel.background = element_blank()
+#   )
+# 
+# patchwork3 <- (p5 / p6) + plot_layout(axis_titles = "collect") | plot_spacer()
 
 ## Education ----
 
-# Compute pay gap 
-paygap_educ <- pay_clean |>
+pay_clean |>
   summarise(meanBase = mean(BasePay),
-            meanBonus = mean(Bonus),
             .by = c(Education, Gender)) |>
   pivot_wider(id_cols = Education,
               names_from = Gender,
-              values_from = c(meanBase, meanBonus)) |>
-  mutate(base_gap = (meanBase_Male - meanBase_Female) / meanBase_Male,
-         bonus_gap = (meanBonus_Male - meanBonus_Female) / meanBonus_Male)
-
-# Bar plots 
-
+              values_from = meanBase) |>
+  mutate(base_gap = (Male - Female) / Male) |> # compute pay gap 
+  ggplot(aes(Education, base_gap, label = percent(base_gap, accuracy = 0.01))) +
+  geom_col() + # graph bar plot
+  geom_text(nudge_y = 0.005) +
+  scale_y_continuous(breaks = seq(0, 1, by = 0.05), 
+                     limits = c(0, 0.2),
+                     expand = c(0, 0),
+                     labels = label_percent(),
+                     name = "Chênh lệch (%)") +
+  labs(title = "Chênh lệch về Lương cơ bản giữa Nam và Nữ theo Học vấn",
+       subtitle = "Chênh lệch = (Lương TB của Nam - Lương TB của Nữ) / Lương TB của Nam * 100%") +
+  theme(
+    panel.border = element_rect(colour = "black"),
+    panel.background = element_blank()
+  )
 
 ## Dept ----
-
+pay_clean |>
+  summarise(meanBase = mean(BasePay),
+            .by = c(Dept, Gender)) |>
+  pivot_wider(id_cols = Dept,
+              names_from = Gender,
+              values_from = meanBase) |>
+  mutate(base_gap = (Male - Female) / Male) |> # compute pay gap 
+  ggplot(aes(Dept, base_gap, label = percent(base_gap, accuracy = 0.01))) +
+  geom_col() + # graph bar plot
+  geom_text(nudge_y = 0.005) +
+  scale_y_continuous(breaks = seq(0, 1, by = 0.05), 
+                     limits = c(0, 0.2),
+                     expand = c(0, 0),
+                     labels = label_percent(),
+                     name = "Chênh lệch (%)") +
+  labs(title = "Chênh lệch về Lương cơ bản giữa Nam và Nữ theo Phòng ban",
+       subtitle = "Chênh lệch = (Lương TB của Nam - Lương TB của Nữ) / Lương TB của Nam * 100%") +
+  theme(
+    panel.border = element_rect(colour = "black"),
+    panel.background = element_blank()
+  )
 
 ## Seniority ----
-
+pay_clean |>
+  summarise(meanBase = mean(BasePay),
+            .by = c(Seniority, Gender)) |>
+  pivot_wider(id_cols = Seniority,
+              names_from = Gender,
+              values_from = meanBase) |>
+  mutate(base_gap = (Male - Female) / Male) |> # compute pay gap 
+  ggplot(aes(Seniority, base_gap, label = percent(base_gap, accuracy = 0.01))) +
+  geom_col() + # graph bar plot
+  geom_text(nudge_y = 0.005) +
+  scale_y_continuous(breaks = seq(0, 1, by = 0.05), 
+                     limits = c(0, 0.2),
+                     expand = c(0, 0),
+                     labels = label_percent(),
+                     name = "Chênh lệch (%)") +
+  labs(title = "Chênh lệch về Lương cơ bản giữa Nam và Nữ theo Bậc Thâm niên",
+       subtitle = "Chênh lệch = (Lương TB của Nam - Lương TB của Nữ) / Lương TB của Nam * 100%") +
+  theme(
+    panel.border = element_rect(colour = "black"),
+    panel.background = element_blank()
+  )
 
 ## PerfEval
+pay_clean |>
+  summarise(meanBase = mean(BasePay),
+            .by = c(PerfEval, Gender)) |>
+  pivot_wider(id_cols = PerfEval,
+              names_from = Gender,
+              values_from = meanBase) |>
+  mutate(base_gap = (Male - Female) / Male) |> # compute pay gap 
+  ggplot(aes(PerfEval, base_gap, label = percent(base_gap, accuracy = 0.01))) +
+  geom_col() + # graph bar plot
+  geom_text(nudge_y = 0.005) +
+  scale_y_continuous(breaks = seq(0, 1, by = 0.05), 
+                     limits = c(0, 0.2),
+                     expand = c(0, 0),
+                     labels = label_percent(),
+                     name = "Chênh lệch (%)") +
+  labs(title = "Chênh lệch về Lương cơ bản giữa Nam và Nữ theo Đánh giá Hiệu suất",
+       subtitle = "Chênh lệch = (Lương TB của Nam - Lương TB của Nữ) / Lương TB của Nam * 100%") +
+  theme(
+    panel.border = element_rect(colour = "black"),
+    panel.background = element_blank()
+  )
