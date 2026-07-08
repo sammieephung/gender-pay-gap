a6c02231b0eaa810f1a679e9e71779a050397a56
Sam Phung <phungnguyenphuong01@gmail.com>
2026-07-08 20:07:12 +1000

    Added html table for overall gender pay gap. Added more thorough comments and refined formatting

--- Rscripts/data_viz.R
+++ Rscripts/data_viz.R
@@ -21,20 +21,16 @@ psych::describe(pay_clean)
 p1 <- ggplot(pay_clean, aes(BasePay)) +
   geom_histogram(binwidth = 5000, col = "black") +
   scale_y_continuous(limits = c(0, 100), expand = c(0, 0)) +
-  theme(
-    panel.background = element_blank(),
-    panel.border = element_rect(colour = "black"),
-    axis.title.y = element_blank()
-  )
+  theme(panel.background = element_blank(),
+        panel.border = element_rect(colour = "black"),
+        axis.title.y = element_blank())
 
 p2 <- ggplot(pay_clean, aes(Bonus)) +
   geom_histogram(binwidth = 500, col = "black") +
   scale_y_continuous(expand = c(0, 0), limits = c(0, 100)) +
-  theme(
-    panel.background = element_blank(),
-    panel.border = element_rect(colour = "black"),
-    axis.title.y = element_blank()
-  )
+  theme(panel.background = element_blank(),
+        panel.border = element_rect(colour = "black"),
+        axis.title.y = element_blank())
 
 patchwork1 <- p1 + p2
 patchwork1 + plot_annotation(title = "Phân bố Lương cơ bản và Lương thưởng")
@@ -43,73 +39,63 @@ patchwork1 + plot_annotation(title = "Phân bố Lương cơ bản và Lương t
 
 ## Box plots ----
 p3 <- ggplot(pay_clean, aes(BasePay, Gender)) +
+  # draw ticks for min/max at the ends of whisker
   stat_boxplot(geom = "errorbar", width = 0.1) +
   geom_boxplot(fill = "lightgrey") +
-  theme(
-    panel.background = element_blank(),
-    panel.border = element_rect(colour = "black")
-  ) 
+  theme(panel.background = element_blank(),
+        panel.border = element_rect(colour = "black")) 
 
 p4 <- ggplot(pay_clean, aes(Bonus, Gender)) +
+  # draw ticks for min/max at the ends of whisker
   stat_boxplot(geom = "errorbar", width = 0.1) +
   geom_boxplot(fill = "lightgrey") +
-  theme(
-    panel.background = element_blank(),
-    panel.border = element_rect(colour = "black")
-  ) 
+  theme(panel.background = element_blank(),
+        panel.border = element_rect(colour = "black")) 
 
+# graph one plot on top of the other
+# merge y-axis titles and add spacer for better aspect ratio
 patchwork2 <- (p3 / p4) + plot_layout(axis_titles = "collect") | plot_spacer()
 patchwork2 + plot_annotation(title = "Phân bố Lương cơ bản và Lương thưởng theo Giới tính")
 
 ## Table of of overall mean pay and pay gap (UNFINISHED) ----
-# pay_clean |>
-#   summarise(meanBase = mean(BasePay),
-#             meanBonus = mean(Bonus),
-#             .by = Gender) |>
-#   pivot_wider(names_from = Gender,
-#               values_from = c(meanBase, meanBonus)) |>
-#   mutate(base_gap = round(((meanBase_Male - meanBase_Female) / meanBase_Male * 100), 2),
-#          bonus_gap = round(((meanBonus_Male - meanBonus_Female) / meanBonus_Male * 100), 2)) |>
-#   pivot_longer(cols = c("meanBase_Male", "meanBonus_Male"),
-#                names_to = "Type",
-#                values_to = "Male",
-#                values_drop_na = T) |>
-#   pivot_longer(cols = c("meanBase_Female", "meanBonus_Female"),
-#                names_to = "Type1",
-#                values_to = "Female") 
-# 
-# overall_gap
-# 
-# overall_gap |>
-#   pivot_longer(cols = c("base_gap", "bonus_gap"),
-#                names_to = "Gap",
-#                values_to = "Type2")
-# 
-#   gt() 
-  # tab_header(
-  #   title = "Trung bình Lương cơ bản theo Giới tính và Chênh lệch (%)") |>
-  # tab_footnote(
-  #   footnote = "Chênh lệch = (Trung bình Nam - Trung bình Nữ) / Trung bình Nam * 100%"
-  # ) |>
-  # tab_spanner(
-  #   label = "Trung bình Lương cơ bản (USD)",
-  #   columns = c(Female, Male)
-  # ) |>
-  # cols_label(
-  #   Female = md("Nữ"),
-  #   Male = md("Nam"),
-  #   overall_gap = md("Chênh lệch (%)")
-  # ) |>
-  # cols_align(align = "center") |>
-  # tab_style(
-  #   style = "vertical-align:middle",
-  #   locations = cells_column_labels(columns = overall_gap)
-  #   ) |>
-  # tab_style(
-  #   style = cell_text(size = "smaller"),
-  #   locations = cells_footnotes()
-  # )
-
+pay_clean |>
+  summarise(meanBase = mean(BasePay),
+            meanBonus = mean(Bonus),
+            .by = Gender) |> # no ungrouping needed
+  # pivot wider to compute pay gaps from the columns
+  pivot_wider(names_from = Gender,
+              values_from = c(meanBase, meanBonus)) |>
+  # compute pay gaps in new columns
+  mutate(base_gap = (meanBase_Male - meanBase_Female) / meanBase_Male,
+         bonus_gap = (meanBonus_Male - meanBonus_Female) / meanBonus_Male) |>
+  # remove "mean" from column names to standardise and prepare for lengthening
+  rename_with(\(x) gsub("mean", "", x) |> tolower()) |>
+  # lengthen for all columns
+  pivot_longer(everything()) |>
+  # separate first column into two
+  separate(col = name, into = c("pay_type", "x"), sep = "_") |>
+  # pivot wider
+  pivot_wider(names_from = x, values_from = value) |>
+  # change pay_type levels for table, convert gap to %, round values to 2 decimal places
+  mutate(pay_type = ifelse(pay_type == "base", "Cơ bản", "Thưởng"),
+         gap = percent(gap, accuracy = 0.01),
+         female = round(female, 2),
+         male = round(male, 2)) |>
+  # create html table
+  gt() |>
+  tab_header(title = "Trung bình Lương (USD) theo Giới tính và Chênh lệch (%)") |>
+  tab_footnote(footnote = "Chênh lệch = (Trung bình Nam - Trung bình Nữ) / Trung bình Nam * 100%") |>
+  tab_spanner(label = "Giới tính",
+              columns = c(female, male)) |>
+  cols_label(pay_type = "Loại lương",
+             female = "Nữ",
+             male = "Nam",
+             gap = "Chênh lệch") |> 
+  cols_align(align = "center") |>
+  tab_style(style = "vertical-align:middle",
+            locations = cells_column_labels(columns = c(pay_type, gap))) |>
+  tab_style(style = cell_text(size = "smaller"),
+            locations = cells_footnotes())
 
 # Gender pay gap by ___ ----
 
@@ -121,13 +107,27 @@ pay_clean$AgeGroup = cut(pay_clean$Age, breaks = c(18, 25, 35, 45, 55, 65, Inf),
                                     "45-54 tuổi", "55-64 tuổi", "Trên 65 tuổi"))
 attr(pay_clean$AgeGroup, "label") <- "Nhóm tuổi"
 
-# Compute pay gap 
-gendergap_age <- pay_clean |>
+
+pay_clean |>
   summarise(mean = mean(BasePay),
             .by = c(AgeGroup, Gender)) |>
   pivot_wider(names_from = Gender,
               values_from = mean) |>
-  mutate(base_gap = (Male - Female) / Male)
+  # Compute pay gap 
+  mutate(base_gap = (Male - Female) / Male) |>
+  # Create bar plots
+  ggplot(aes(x = AgeGroup, y = base_gap, label = percent(base_gap))) +
+  geom_col() +
+  geom_text(nudge_y = 0.005) +
+  scale_y_continuous(breaks = seq(0, 1, by = 0.05), 
+                     limits = c(0, 0.2),
+                     expand = c(0, 0),
+                     labels = label_percent(),
+                     name = "Chênh lệch (%)") +
+  labs(title = "Chênh lệch về Lương cơ bản giữa Nam và Nữ theo Nhóm tuổi",
+       subtitle = "Chênh lệch = (Lương TB của Nam - Lương TB của Nữ) / Lương TB của Nam * 100%") +
+  theme(panel.border = element_rect(colour = "black"),
+        panel.background = element_blank())
 
 # meanbonus_age <- pay_clean |>
 #   summarise(mean = mean(Bonus),
@@ -143,25 +143,7 @@ gendergap_age <- pay_clean |>
 #               values_from = mean) |>
 #   mutate(total_gap = (Male - Female) / Male)
 
-# Create bar plots
-p5 <- ggplot(gendergap_age, aes(x = AgeGroup, y = base_gap, label = percent(base_gap))) +
-  geom_col() +
-  geom_text(nudge_y = 0.005) +
-  scale_y_continuous(breaks = seq(0, 1, by = 0.05), 
-                     limits = c(0, 0.2),
-                     expand = c(0, 0),
-                     labels = label_percent(),
-                     name = "Chênh lệch (%)") +
-  labs(title = "Chênh lệch về Lương cơ bản giữa Nam và Nữ theo Nhóm tuổi",
-       subtitle = "Chênh lệch = (Lương TB của Nam - Lương TB của Nữ) / Lương TB của Nam * 100%") +
-  theme(
-    panel.border = element_rect(colour = "black"),
-    panel.background = element_blank()
-  )
-
-p5
-
-# p6 <- ggplot(gendergap_age, aes(x = AgeGroup, y = bonus_gap)) +
+# ggplot(meanbonus_age, aes(x = AgeGroup, y = bonus_gap)) +
 #   geom_col() +
 #   scale_y_continuous(breaks = seq(-0.2, 0.2, by = 0.05), 
 #                      limits = c(-0.2, 0.2),
@@ -197,10 +179,8 @@ pay_clean |>
                      name = "Chênh lệch (%)") +
   labs(title = "Chênh lệch về Lương cơ bản giữa Nam và Nữ theo Học vấn",
        subtitle = "Chênh lệch = (Lương TB của Nam - Lương TB của Nữ) / Lương TB của Nam * 100%") +
-  theme(
-    panel.border = element_rect(colour = "black"),
-    panel.background = element_blank()
-  )
+  theme(panel.border = element_rect(colour = "black"),
+        panel.background = element_blank())
 
 ## Dept ----
 pay_clean |>
@@ -220,10 +200,8 @@ pay_clean |>
                      name = "Chênh lệch (%)") +
   labs(title = "Chênh lệch về Lương cơ bản giữa Nam và Nữ theo Phòng ban",
        subtitle = "Chênh lệch = (Lương TB của Nam - Lương TB của Nữ) / Lương TB của Nam * 100%") +
-  theme(
-    panel.border = element_rect(colour = "black"),
-    panel.background = element_blank()
-  )
+  theme(panel.border = element_rect(colour = "black"),
+        panel.background = element_blank())
 
 ## Seniority ----
 pay_clean |>
@@ -243,10 +221,8 @@ pay_clean |>
                      name = "Chênh lệch (%)") +
   labs(title = "Chênh lệch về Lương cơ bản giữa Nam và Nữ theo Bậc Thâm niên",
        subtitle = "Chênh lệch = (Lương TB của Nam - Lương TB của Nữ) / Lương TB của Nam * 100%") +
-  theme(
-    panel.border = element_rect(colour = "black"),
-    panel.background = element_blank()
-  )
+  theme(panel.border = element_rect(colour = "black"),
+        panel.background = element_blank())
 
 ## PerfEval
 pay_clean |>
@@ -266,7 +242,5 @@ pay_clean |>
                      name = "Chênh lệch (%)") +
   labs(title = "Chênh lệch về Lương cơ bản giữa Nam và Nữ theo Đánh giá Hiệu suất",
        subtitle = "Chênh lệch = (Lương TB của Nam - Lương TB của Nữ) / Lương TB của Nam * 100%") +
-  theme(
-    panel.border = element_rect(colour = "black"),
-    panel.background = element_blank()
-  )
+  theme(panel.border = element_rect(colour = "black"),
+        panel.background = element_blank())
