5960c248b29628e4dcd6c5b39e4679be4d4d23f6
Sam Phung <phungnguyenphuong01@gmail.com>
2026-08-10 16:16:56 +0700

    Refined based on data management principles, edited README

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # Bất bình đẳng thu nhập theo giới tính (Gender pay gap)
 My first data analysis project in R as part of coursework for HUSO2313: Analysis &amp; Communication of Social Science Research at RMIT University.
 
+## Repository Structure
+
+
+
+## Data Dictionary
+
+
+
+| Filename          | File description                                                                                                |
+|-------------------|-----------------------------------------------------------------------------------------------------------------|
+| pay_raw.xlsx      | Original data set in Excel format, containing 1,000  observations as provided by Kaggle                         |
+| pay_cleanv1.RData | First version of the clean data set in .RData format, with 989 observations and Vietnamese labels for variables |
--- Rscripts/data_cleaning.R
+++ Rscripts/data_cleaning.R
@@ -1,140 +1,186 @@
 # Load necessary packages through package manager "pacman" ----
 
-if(!require("pacman")) install.packages("pacman")
-pacman::p_load(tidyverse, # for data wrangling
-               readxl, # to import data from Excel files
-               here, # to specify file path relative to project directory
-               psych, # for descriptive stats
-               naniar, # to assess missing data 
-               table1, tableone) # for descriptive stats table
+if (!require("pacman")) {
+  install.packages("pacman")
+}
+pacman::p_load(
+  tidyverse, # for data wrangling
+  readxl, # to import data from Excel files
+  here, # to specify file path relative to project directory
+  psych, # for descriptive stats
+  naniar, # to assess missing data
+  table1,
+  tableone
+) # for descriptive stats table
 
 # Import data set ----
 
-pay_v1 = read_xlsx(path = here::here("data", "PayV1.xlsx"), col_names = T)
+pay_raw <- read_xlsx(path = here::here("data", "pay_raw.xlsx"), col_names = T)
 
 # DATA CLEANING ----
 
 # inspect raw data
-str(pay_v1)
+str(pay_raw)
 # BasePay is inputted as characters indicating possible data entry error
 
 ## Initial Missing Data ----
-sapply(pay_v1, function(x) sum(is.na(x))) # number of NAs in each variable
+sapply(pay_raw, function(x) sum(is.na(x))) # number of NAs in each variable
+naniar::miss_var_summary(pay_raw)
 
 ## Invalid entries ----
 
 # list all unique values under each categorical variable
-unique(pay_v1$JobTitle)
-unique(pay_v1$Gender) # "."
-unique(pay_v1$Education)
-unique(pay_v1$Dept) # "."
-unique(pay_v1$Seniority)
-unique(pay_v1$PerfEval) # "10"
+cat_vars <- pay_raw[, c(
+  "JobTitle",
+  "Gender",
+  "Education",
+  "Dept",
+  "Seniority",
+  "PerfEval"
+)]
+sapply(cat_vars, function(x) unique(x))
 
-# check each quantitative variable for non-digits in values
-pay_v1 |>
+# check each quantitative variable for NA/non-digit entries
+pay_raw |>
   filter(str_detect(BasePay, "\\D") | is.na(BasePay)) # "100463P"
-pay_v1 |>
-  filter(str_detect(Age, "\\D") | is.na(BasePay))
-pay_v1 |>
+pay_raw |>
+  filter(str_detect(Age, "\\D") | is.na(Age))
+pay_raw |>
   filter(str_detect(Bonus, "\\D") | is.na(Bonus))
 
-## Treat dot values as NAs ----
-pay_v1 <- pay_v1 |> naniar::replace_with_na_all(condition = ~.x == ".")
+## treat dot values as NAs ----
+pay_raw <- pay_raw |> naniar::replace_with_na_all(condition = ~ .x == ".")
 
-# number of NAs
-naniar::miss_var_summary(pay_v1) # in each variable
-naniar::miss_case_summary(pay_v1) # in individual cases
+# re-count NAs
+naniar::miss_var_summary(pay_raw) # in each variable
+naniar::miss_case_summary(pay_raw) # in individual cases
 
-# Little's missing completely at random test 
-mcar_test(pay_v1) # p = .598, thus data is MCAR
+# Little's missing completely at random test
+mcar_test(pay_raw) # p = .598, thus data is MCAR
 
 ## Remove all NAs & invalid entries ----
-pay_clean <- pay_v1 |>
+pay_raw <- pay_raw |>
   drop_na() |> # 7 cases dropped
   filter_out(BasePay == "100463P" | PerfEval == "10") # 2 invalids
 
 # DATA WRANGLING ----
 
 ## Change all variables into appropriate type based on codebook ----
-str(pay_clean) # initial data structure
+str(pay_raw) # initial data structure
 
-pay_clean = pay_clean |>
-  mutate(JobTitle = as.factor(JobTitle),
-         Gender = as.factor(Gender), # Female will be ref
-         Education = factor(Education, levels = c("High School", "College", "Masters", "PhD")),
-         Dept = as.factor(Dept), 
-         Seniority = factor(Seniority, levels = c("1", "2", "3", "4", "5")),
-         BasePay = as.numeric(BasePay),
-         Age = as.numeric(Age),
-         Bonus = as.numeric(Bonus), 
-         PerfEval = factor(PerfEval, levels = c("1", "2", "3", "4", "5"))
-         )
+pay_raw <- pay_raw |>
+  mutate(
+    JobTitle = as.factor(JobTitle),
+    Gender = as.factor(Gender), # Female will be ref
+    Education = factor(
+      Education,
+      levels = c("High School", "College", "Masters", "PhD")
+    ),
+    Dept = as.factor(Dept),
+    Seniority = factor(Seniority, levels = c("1", "2", "3", "4", "5")),
+    BasePay = as.numeric(BasePay),
+    Age = as.numeric(Age),
+    Bonus = as.numeric(Bonus),
+    PerfEval = factor(PerfEval, levels = c("1", "2", "3", "4", "5"))
+  )
 
 ## Descriptive stats for quantitative variables ----
-describe(pay_clean[, c("BasePay", "Age", "Bonus")])
-# BasePay & Bonus are heavily skewed by absurd max values 
+describe(pay_raw[, c("BasePay", "Age", "Bonus")])
+# BasePay & Bonus are heavily skewed by absurd max values
 # max Age also does not make sense
 
 ## Examine outliers ----
 par(mfrow = c(1, 3)) # for side-by-side boxplots
-attach(pay_clean)
+attach(pay_raw)
 boxplot(BasePay)
 boxplot(Age)
 boxplot(Bonus)
 
 # max and mean for each quant variables
-cbind(max_BasePay = max(BasePay),
-      max_Age = max(Age),
-      max_Bonus = max(Bonus))
-cbind(mean_BasePay = mean(BasePay),
-      mean_Age = mean(Age),
-      mean_Bonus = mean(Bonus))
+cbind(max_BasePay = max(BasePay), max_Age = max(Age), max_Bonus = max(Bonus))
+cbind(
+  mean_BasePay = mean(BasePay),
+  mean_Age = mean(Age),
+  mean_Bonus = mean(Bonus)
+)
 
 # correct/remove outliers
-pay_clean <-  pay_clean |>
+pay_raw <- pay_raw |>
   mutate(BasePay = replace_values(BasePay, 670890 ~ 67089)) |>
+  # remove last 0 digit at the end from BasePay outlier
   filter_out(Age == max(Age) | Bonus == max(Bonus))
+# remove Age outlier and Bonus outlier
 
 # re-check after correction/removal
-attach(pay_clean)
+attach(pay_raw)
 boxplot(BasePay)
 boxplot(Age)
 boxplot(Bonus)
 
-describe(pay_clean[, c("BasePay", "Age", "Bonus")]) # looks better now
+describe(pay_raw[, c("BasePay", "Age", "Bonus")]) # looks better now
+
+# Save cleaned data set to .RData file ----
+pay_cleanv1 <- pay_raw
+save(pay_cleanv1, file = here::here("data", "pay_cleanv1.RData"))
 
 # Table 1 Descriptive Statistics ----
 
 ## tableone package ----
-table1 = tableone::CreateTableOne(vars = c("JobTitle",  "Age", "Education", "Dept", 
-                                  "Seniority", "BasePay", "Bonus", "PerfEval"),
-                         strata = "Gender", data = pay_clean, test = F, addOverall = T)
-# kableone(table1) # html table
-print(table1)
+tableone::CreateTableOne(
+  vars = c(
+    "JobTitle",
+    "Age",
+    "Education",
+    "Dept",
+    "Seniority",
+    "BasePay",
+    "Bonus",
+    "PerfEval"
+  ),
+  strata = "Gender",
+  data = pay_cleanv1,
+  test = F,
+  addOverall = T
+) |>
+  print() |>
+  # kableone() # html
 
-## table1 package ----
+  ## table1 package ----
 
-# set labels for each variable
-pay_clean <- pay_clean |>
-  mutate(JobTitle = setLabel(JobTitle, "Vị trí"),
-         Gender = setLabel(Gender, "Giới tính"),
-         Age = setLabel(Age, "Tuổi"),
-         Education = setLabel(Education, "Học vấn"),
-         Dept = setLabel(Dept, "Phòng ban"), 
-         Seniority = setLabel(Seniority, "Thâm niên (bậc)"),
-         BasePay = setLabel(BasePay, "Lương cơ bản (USD)"),
-         Bonus = setLabel(Bonus, "Lương thưởng (USD)"),
-         PerfEval = setLabel(PerfEval, "Đánh giá Hiệu suất"))
+  # set labels for each variable
+  pay_cleanv1 <- pay_cleanv1 |>
+  mutate(
+    JobTitle = setLabel(JobTitle, "Vị trí"),
+    Gender = setLabel(Gender, "Giới tính"),
+    Age = setLabel(Age, "Tuổi"),
+    Education = setLabel(Education, "Học vấn"),
+    Dept = setLabel(Dept, "Phòng ban"),
+    Seniority = setLabel(Seniority, "Thâm niên (bậc)"),
+    BasePay = setLabel(BasePay, "Lương cơ bản (USD)"),
+    Bonus = setLabel(Bonus, "Lương thưởng (USD)"),
+    PerfEval = setLabel(PerfEval, "Đánh giá Hiệu suất")
+  )
+
+# re-save data
+save(pay_cleanv1, file = here::here("data", "pay_cleanv1.RData"))
 
 # set customised render for quantitative var
-my.render.cont <- c("Trung bình (Lệch chuẩn)"=sprintf("%s (%s)", "MEAN", "SD"))
+my.render.cont <- c(
+  "Trung bình (Lệch chuẩn)" = sprintf("%s (%s)", "MEAN", "SD")
+)
 
-table1::table1(~ JobTitle + Age + Education + Dept + 
-                 Seniority + BasePay + Bonus + PerfEval | Gender, 
-               data = pay_clean, render.continuous = my.render.cont)
+table1::table1(
+  ~ JobTitle +
+    Age +
+    Education +
+    Dept +
+    Seniority +
+    BasePay +
+    Bonus +
+    PerfEval |
+    Gender,
+  data = pay_cleanv1,
+  render.continuous = my.render.cont
+)
 # html table in Viewer pane
-# also gives median + min/max for quant variables
-
-# Save cleaned data set to .RData file ----
-save(pay_clean, file = here::here("data", "pay_clean.RData"))
+# default output gives median + min/max for quant variables
